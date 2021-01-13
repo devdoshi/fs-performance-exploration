d6692b9ac123fc262060e3ac99e84b870ab7d7ad
Dev Doshi <dev@useoptic.com>
2021-01-13 12:13:43 -0500

    read more per iteration

--- nodejs/index.ts
+++ nodejs/index.ts
@@ -4,7 +4,11 @@ async function main(options: { logPrefix: string, filePath: string }) {
   const key = `${logPrefix}-read`;
   console.log(`${key}-start`)
   console.time(key);
-  await fs.readFile(options.filePath);
+  for (let i = 0; i < 3; i++) {
+    console.time(`${key}-${i}`);
+    await fs.readFile(options.filePath);
+    console.timeEnd(`${key}-${i}`);
+  }
   console.timeEnd(key);
   console.log(`${key}-end`)
 }
